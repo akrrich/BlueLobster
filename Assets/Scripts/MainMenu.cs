5fad3d836bdbb431ed533f73446bb7b73b27d9be
akrrich <ayronovo@gmail.com>
2024-12-26 03:48:23 -0300

    MainMenu
    Botones de jugar opciones y salir funcionando correctamente con sonido y música de fondo.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject panelSettings;
+
+    private AudioSource buttonClick;
+
+
+    void Start()
+    {
+        GetComponents();
+    }
+
+
+    public void ButtonPlayGame()
+    {
+        StartCoroutine(LoadSceneAfterButtonClick());
+    }
+
+    public void ButtonSettings()
+    {
+        buttonClick.Play();
+        panelSettings.SetActive(true);
+    }
+
+    public void ButtonExit()
+    {
+        StartCoroutine(CloseGameAfterClickButton());
+    }
+
+    public void ButtonBack()
+    {
+        buttonClick.Play();
+        panelSettings.SetActive(false);
+    }
+
+
+    private void GetComponents()
+    {
+        buttonClick = GetComponent<AudioSource>();
+    }
+
+    private IEnumerator LoadSceneAfterButtonClick()
+    {
+        buttonClick.Play();
+
+        yield return new WaitForSeconds(buttonClick.clip.length);
+
+        SceneManager.LoadSceneAsync("Game");
+    }
+
+    private IEnumerator CloseGameAfterClickButton()
+    {
+        buttonClick.Play();
+
+        yield return new WaitForSeconds(buttonClick.clip.length);
+
+        Application.Quit();
+    }
+}
